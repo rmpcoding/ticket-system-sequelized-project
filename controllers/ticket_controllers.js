5569e13b89c335681c6be034a920d398352d7404
Robert Parsons <rmpcoding@gmail.com>
2020-09-17 14:33:40 -0700

    Adds 'hello world' functionality to controllers linking to views (routes to handlebars)

---
+++ controllers/ticket_controllers.js
@@ -0,0 +1,7 @@
+const Router = require('express').Router()
+
+Router.get('/', (req, res) => {
+  res.render('index')
+})
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
 const { static } = require('express');
-const Routes = require('./controllers/ticket_controllers')
+const Routes = require('./controllers/ticket_controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
