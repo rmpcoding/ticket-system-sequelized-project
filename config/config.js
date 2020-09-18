59ca11be4858b749d51efc707d546d27096fe569
Robert Parsons <rmpcoding@gmail.com>
2020-09-17 22:51:21 -0700

    Adds config.js file instead of config.json file--works now with .env file to safeguard passwords and other local vars

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "sequelize_ticket",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB,
     "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
--- controllers/ticket_controllers.js
+++ controllers/ticket_controllers.js
@@ -2,10 +2,10 @@ const Router = require('express').Router()
 const db = require('../models')
 
 Router.get('/', (req, res) => {
-  db.Ticket.findAll({}).then(function(dbTicket) {
-    // We have access to the todos as an argument inside of the callback function
-    res.json(dbTicket);
-  });
+  // db.Ticket.findAll({}).then(function(dbTicket) {
+  //   // We have access to the todos as an argument inside of the callback function
+  //   res.json(dbTicket);
+  // });
 })
 
 module.exports = Router;
